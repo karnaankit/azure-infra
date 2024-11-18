e609842d89acd00e855fcf9ea62e70c24b327806
karna256 <ankitkarna@lftechnology.com>
2024-11-18 18:47:20 +0545

    export arm secrets

--- remote-state-storage.tf
+++ remote-state-storage.tf
@@ -16,9 +16,9 @@ terraform {
 provider "azurerm" {
   features {}
   subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
+  # tenant_id       = var.tenant_id
+  # client_id       = var.client_id
+  # client_secret   = var.client_secret
 }
 
 resource "azurerm_resource_group" "tfstate" {
